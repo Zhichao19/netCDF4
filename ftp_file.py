8919c4f26aaeec2511347c4477a9cd88f89f603e
Zhichao19 <15251215@bjtu.edu.cn>
2023-07-11 16:12:46 +0100

    example

--- ftp_file.py
+++ ftp_file.py
@@ -25,7 +25,7 @@ f.login("zw2023", "iGDdEi0Q0?bP")
 
 # %% 
 # loop through years
-for year in range(2000,2001):
+for year in range(1960,1961):
 
     # loop through months
     for month in range(1,13):
@@ -37,20 +37,50 @@ for year in range(2000,2001):
             ndays=int("dummy 31 28 31 30 31 30 31 31 30 31 30 31".split()[month])
 
         # loop through days
-        for day in range(1, ndays+1):
+        # for day in range(1, ndays+1):
 
             # loop through hours
-            for hour in range(0, 19, 6):
+            # for hour in range(0, 19, 6):
 
                 # loop through variables
-                for var in ("10u", "10v"):
+                # for var in ("10u", "10v"):
 
-                    # change the remote directory
-                    f.cwd("/badc/ecmwf-e40/data/gg/as/%.4d/%.2d/%.2d" % (year, month, day))
-                    # define filename
-                    file="ggas%.4d%.2d%.2d%.2d%s.grb" % (year, month, day, hour, var)
-                    # get the remote file to the local directory
-                    f.retrbinary("RETR %s" % file, open(file, "wb").write)
+        # change the remote directory
+        # f.cwd("/badc/ecmwf-e40/data/gg/as/%.4d/%.2d/%.2d" % (year, month, day))
+        f.cwd("/badc/ukmo-hadobs/data/insitu/MOHC/HadOBS/HadUK-Grid/v1.1.0.0/1km/tasmax/day/v20220310")
+        
+        # define filename
+        # file="ggas%.4d%.2d%.2d%.2d%s.grb" % (year, month, day, hour, var)
+        file="tasmax_hadukgrid_uk_1km_day_1960%.2d01-1960%.2d%.2d.nc" % (month, month, ndays)
+        # get the remote file to the local directory
+        f.retrbinary("RETR %s" % file, open(file, "wb").write)
 
 # Close FTP connection
 f.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
